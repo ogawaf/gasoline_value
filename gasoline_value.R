5aee338e035e3646e74195c6a95b00a928e1c6a7
ogwf <fukutugu@yahoo.co.jp>
2017-10-24 00:51:48 +0900

    add latest 1 month changes in gasoline prices plot

--- gasoline_value.R
+++ gasoline_value.R
@@ -39,4 +39,17 @@ latest_survey_date <-
 # レギュラーガソリンの価格推移
 ggplot(gasoline) +
   geom_path(aes(x = 調査日, y = 全国)) +
-  labs(title = paste0("最新調査日：", latest_survey_date))
+  labs(title = paste0("これまでのガソリン価格の推移   最新調査日：", latest_survey_date))
+ggsave("change_in_gasoline_prices.png")
+
+# 直近一か月のガソリン価格の推移
+gasoline %>% 
+  arrange(desc(調査日)) %>% 
+  slice(1:52) %>% 
+  ggplot(aes(x = 調査日, y = 全国)) +
+  geom_path() +
+  scale_x_date(date_breaks = "4 week") +
+  labs(title = paste0("直近52週(約1年)のガソリン価格の推移  最新調査日：", latest_survey_date))  +
+  theme(axis.text.x = element_text(angle = 30, vjust = .5))
+ggsave("change_in_gasoline_prices(latest_1year).png")
+
--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 
 資源エネルギー庁の[HP](http://www.enecho.meti.go.jp/statistics/petroleum_and_lpgas/pl007/results.html)から給油所小売価格調査（ガソリン、軽油、灯油）の週次データを取得し、
 レギュラーガソリン価格の推移を作図する。
+
+![これまでのレギュラーガソリンの価格推移](change_in_gasoline_prices.png)
+
+![直近52週（約1年）のレギュラーガソリンの価格推移](change_in_gasoline_prices%28latest_1year%29.png)
